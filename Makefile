815ed2bef2b50650e6848b7be8c914c4b612f2a5
slavanr45 <slava.r45@gmail.com>
2023-11-02 22:30:38 +0700

    add gunicorn and dotenv

--- Makefile
+++ Makefile
@@ -4,6 +4,10 @@ dev:
 dev_debug:
 	poetry run flask --app page_analyzer:app --debug run
 
+PORT ?= 8000
+start:
+	poetry run gunicorn -w 5 -b 0.0.0.0:$(PORT) page_analyzer:app
+
 install:
 	poetry install
 
--- page_analyzer/app.py
+++ page_analyzer/app.py
@@ -1,8 +1,15 @@
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
 app = Flask(__name__)
-app.secret_key = "secret_key"
+# загрузка переменных окружения из скрытого файла
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+    app.secret_key = os.getenv('SECRET')
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return f'{app.secret_key}'
