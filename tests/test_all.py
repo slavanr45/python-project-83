b5a17715f9a1a784810668d9a40386dabd8d9b8e
slavanr45 <slava.r45@gmail.com>
2023-11-04 21:33:04 +0700

    improve test coverage

--- tests/test_all.py
+++ tests/test_all.py
@@ -3,4 +3,4 @@ from page_analyzer.app import index, dotenv_path
 
 def test_first():
     assert index() == 'Hello world!'
-    assert dotenv_path == '/home/slava/project3/page_analyzer/.env'
+    assert dotenv_path
