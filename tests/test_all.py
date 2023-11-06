ea95925d81d4f3847e327e0b0a63cb9ec90386a7
slavanr45 <slava.r45@gmail.com>
2023-11-06 23:06:19 +0700

    add conftest.py

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,30 +1,5 @@
-import pytest
-from page_analyzer.app import test1, app as create_app
+from page_analyzer.app import test1
 
 
 def test_first():
     assert test1 == 'test1'
-
-
-@pytest.fixture()
-def app():
-    app = create_app()
-    app.config.update({
-        "TESTING": True,
-    })
-
-    # other setup can go here
-    yield app
-    # clean up / reset resources here
-
-
-@pytest.fixture()
-def client(app):
-    """A test client for the app."""
-    return app.test_client()
-
-
-@pytest.fixture()
-def runner(app):
-    """A test runner for the app's Click commands."""
-    return app.test_cli_runner()
