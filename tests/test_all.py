04e6eeb4580e1969398b0cbe8bc536898be95041
slavanr45 <slava.r45@gmail.com>
2023-11-06 23:00:13 +0700

    add tests

--- tests/test_all.py
+++ tests/test_all.py
@@ -1,5 +1,30 @@
-from page_analyzer.app import test1
+import pytest
+from page_analyzer.app import test1, app as create_app
 
 
 def test_first():
     assert test1 == 'test1'
+
+
+@pytest.fixture()
+def app():
+    app = create_app()
+    app.config.update({
+        "TESTING": True,
+    })
+
+    # other setup can go here
+    yield app
+    # clean up / reset resources here
+
+
+@pytest.fixture()
+def client(app):
+    """A test client for the app."""
+    return app.test_client()
+
+
+@pytest.fixture()
+def runner(app):
+    """A test runner for the app's Click commands."""
+    return app.test_cli_runner()
