19622f69b4f86004fbed32573a60f4463d7cd340
slavanr45 <slava.r45@gmail.com>
2023-11-15 22:17:39 +0700

    testing-2

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@
 # Postgres позволяет подключиться к удаленной базе указав ссылку на нее после флага -d
 # ссылка подгрузится из переменной окружения, которую нужно указать на сервисе деплоя
 # дальше мы загружаем в поключенную базу наш sql-файл с таблицами
-make install && psql -a -d $DATABASE_URL -f database.sql
+make install && psql -a -d postgres://postgres_rmev_user:wXSIbr4wkvtYeqF4wF3urGFVJiLZi6t1@dpg-clac5p62eqrc7398jsug-a/postgres_rmev -f database.sql
