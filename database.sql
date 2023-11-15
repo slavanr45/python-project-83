cd3ccc2c0b0fa5efda68a88b1d719fcf6c285508
slavanr45 <slava.r45@gmail.com>
2023-11-15 21:13:53 +0700

    fix database.sql

--- database.sql
+++ database.sql
@@ -1,8 +1,5 @@
-DROP TABLE IF EXISTS urls;
 CREATE TABLE urls (
     id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
     name varchar(255),
     created_at timestamp
 );
-
-INSERT INTO urls (name) VALUES ('Bash'), ('PHP'), ('Ruby'), ('test');
